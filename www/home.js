5be8d1a0a9931e268b0f1c04e14f2556dbe2f31f
akshayarora2009 <akshayarora2009@gmail.com>
2015-06-30 21:48:04 +0530

    Updated location based results

--- www/home.js
+++ www/home.js
@@ -1,19 +1,80 @@
-﻿
+﻿var point;
+document.addEventListener("deviceready", onDeviceReady, false);
+
+
+
+function onDeviceReady(){
+
+
+cordova.plugins.diagnostic.isLocationEnabled(function(enabled){
+    //alert("Location is " + (enabled ? "enabled" : "disabled"));
+    if(!enabled)
+    {
+	alert('Location is disabled');
+        cordova.plugins.diagnostic.switchToLocationSettings();
+return;
+    }
+else
+{
+ 	alert('Location is enabled');
+
+}
+
+	var onSuccess = function(position) {
+point = new Parse.GeoPoint({latitude: position.coords.latitude, longitude: position.coords.longitude});
+
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+ReactCode();
+};
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
+
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+
+
+
+});
 
 
 
 
 
 
+
+}
+
+
+function ReactCode(){
 
 var OfferListings = React.createClass({
 
     mixins: [ParseReact.Mixin],
 
+
+
     observe: function(){
 
+
+
+
+
+
+
         
-        var off = (new Parse.Query('OffersLive').include("PostedBy"));
+        var off = (new Parse.Query('OffersLive').include("PostedBy").near("pos",point).withinKilometers("pos",point,500));
  
 
         return {
@@ -38,18 +99,7 @@ var OfferListings = React.createClass({
                 this.data.offers.map(function(c){
                     console.log(c);
                     
-                    /*var PlaceName;
-                    var tempObj = Parse.Object.extend("masterList"); 
-                    var query = new Parse.Query(tempObj);
-                    query.equalTo("objectId",c.PostedBy.objectId);
-                    query.find({
-                        success: function(postedby){
-                            PlaceName = (postedby[0].get("PlaceName"));
-                            
-                        }
 
-                    });
-                    */
                     return <li key={c.objectId} className="list-group-item">{c.Title}<br/>{c.PostedBy.PlaceName}</li>
                     
                 })
@@ -74,3 +124,7 @@ React.render(
 
     );
 
+
+
+
+}
